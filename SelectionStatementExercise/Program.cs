0b9e4175335f69bbe2a60e1150b9d952006d8401
Khang Huynh <kkyhuynh@gmail.com>
2024-12-05 11:22:04 -0600

    Selection Statement 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 11);
+            int response = 0;
+
+            while (response != favNumber)
+            {
+                Console.WriteLine("Give me a number between 1-10:");
+                response = int.Parse(Console.ReadLine());
+
+                if (response < favNumber)
+                {
+                    Console.WriteLine($"Too Low!");
+                }
+                else if (response > favNumber)
+                {
+                    Console.WriteLine($"Too High!");
+                }
+                else
+                {
+                    Console.WriteLine($"You're a beast!");
+                } 
+            }
+
+          
         }
     }
 }
